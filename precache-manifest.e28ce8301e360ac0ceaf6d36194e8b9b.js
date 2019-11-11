e6850ab07f2586619822b4a158aa269aede8695b
Alex De Sousa <alexndesousa@hotmail.co.uk>
2019-11-11 19:31:12 +0000

    Updates

---
+++ precache-manifest.e28ce8301e360ac0ceaf6d36194e8b9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d167798ac2ab9b93b5f758e5077c2de8",
+    "url": "/simple-discovery/index.html"
+  },
+  {
+    "revision": "a193bf434d1737067502",
+    "url": "/simple-discovery/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a763f0d78ee49c299daf",
+    "url": "/simple-discovery/static/js/2.f93065dd.chunk.js"
+  },
+  {
+    "revision": "a193bf434d1737067502",
+    "url": "/simple-discovery/static/js/main.d8b81688.chunk.js"
+  },
+  {
+    "revision": "0de49a525d7f9080401c",
+    "url": "/simple-discovery/static/js/runtime-main.bcd95950.js"
+  },
+  {
+    "revision": "f7c55a13b332d65bdf0a425e521b4b88",
+    "url": "/simple-discovery/static/media/placeholder.f7c55a13.png"
+  }
+]);
